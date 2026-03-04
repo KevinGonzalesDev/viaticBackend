238d04f06e6ed375d7751cc1f3d30367afbb7629
kgonzales <kevinpiero00@gmail.com>
2026-03-04 11:08:28 -0500

    fix port usage

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 // src/server.js
 import app from './app.js'
-import { env } from '../config/env.js'
+import { env } from './config/env.js'
 
-const PORT = env.PORT
+const PORT = env.PG_PORT
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`)
